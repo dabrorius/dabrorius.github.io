969601a48c61ad3f890c4c23dd4f059a5d91db6c
Filip Defar <filip@booqable.com>
2024-06-02 08:10:55 +0200

    Add basic CSS

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPassthroughCopy("bundle.css");
 };
